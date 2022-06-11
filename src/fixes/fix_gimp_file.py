fdfcb1e6ca1f424dc31849c66ad4d6a3ec36a5bd
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-06-11 10:35:30 +0700

    Updated the gimp file fix script

--- src/fixes/fix_gimp_file.py
+++ src/fixes/fix_gimp_file.py
@@ -6,9 +6,11 @@ animal = 'DK78'
 DIR = f'/net/birdstore/Active_Atlas_Data/data_root/pipeline_data/{animal}/preps'
 INPUT = os.path.join(DIR, 'CH1', 'thumbnail_cleaned')
 
-badfile = '181.tif'
-badpath = os.path.join(INPUT, badfile)
-badarr = io.imread(badpath)
-print(badfile,badarr.dtype, badarr.shape)
 
-io.imsave(badpath, badarr)
+
+badfiles = ['261.tif','262.tif','263.tif','264.tif','265.tif','266.tif','267.tif']
+for badfile in badfiles:
+    badpath = os.path.join(INPUT, badfile)
+    badarr = io.imread(badpath)
+    print(badfile,badarr.dtype, badarr.shape)
+    io.imsave(badpath, badarr)
