798f2b6a001baadcb86d8dbe63018f39cb3aadae
Edward O'Donnell <eddy.odonnell@gmail.com>
2022-08-18 15:27:52 +0700

    Updated mask trainer from readthedocs

--- src/masking/mask_trainer.py
+++ src/masking/mask_trainer.py
@@ -92,7 +92,7 @@ class MaskDataset(torch.utils.data.Dataset):
 
 def get_model(num_classes):
     # load an object detection model pre-trained on COCO
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
     # get the number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     # replace the pre-trained head with a new on
@@ -102,18 +102,19 @@ def get_model(num_classes):
 
 
 def get_model_instance_segmentation(num_classes):
-    # load an instance segmentation model pre-trained pre-trained on COCO
+    # load an instance segmentation model pre-trained on COCO
     model = torchvision.models.detection.maskrcnn_resnet50_fpn(weights="DEFAULT")
-    #model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
     # get number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     # replace the pre-trained head with a new one
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
     # now get the number of input features for the mask classifier
     in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
     hidden_layer = 256
     # and replace the mask predictor with a new one
     model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask, hidden_layer, num_classes)
+
     return model
 
 def get_transform(train):
@@ -124,16 +125,44 @@ def get_transform(train):
         transforms.append(T.RandomHorizontalFlip(0.5))
     return T.Compose(transforms)
 
+def test_model(ROOT, animal):
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(weights="DEFAULT")
+    dataset = MaskDataset(ROOT, animal, transforms = get_transform(train=True))
+    data_loader = torch.utils.data.DataLoader(
+    dataset, batch_size=2, shuffle=True, num_workers=2,
+    collate_fn=utils.collate_fn)
+    # For Training
+    images,targets = next(iter(data_loader))
+    images = list(image for image in images)
+    targets = [{k: v for k, v in t.items()} for t in targets]
+    output = model(images,targets)   # Returns losses and detections
+    print('Output')
+    print(output)
+    # For inference
+    model.eval()
+    x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
+    predictions = model(x)           # Returns predictions
+    print()
+    print('Predictions')
+    print(predictions)
+    print()
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Work on Animal')
     parser.add_argument('--animal', help='specify animal', required=False)
     parser.add_argument('--runmodel', help='run model', required=True)
+    parser.add_argument('--debug', help='test model', required=False, default='false')
     parser.add_argument('--epochs', help='# of epochs', required=False, default=2)
     
     args = parser.parse_args()
     runmodel = bool({'true': True, 'false': False}[args.runmodel.lower()])
+    debug = bool({'true': True, 'false': False}[args.debug.lower()])
+
     animal = args.animal
     epochs = int(args.epochs)
+    if debug:
+        test_model(ROOT, animal)
+        sys.exit()
 
     dataset = MaskDataset(ROOT, animal, transforms = get_transform(train=True))
     dataset_test = MaskDataset(ROOT, animal, transforms = get_transform(train=False))
